6f3a3ab4fb59e9b5b92169015b1ceef4911f5869
Paul Holstein <paulholstein02@gmail.com>
2023-08-09 15:08:25 +0200

    work on log-in screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogInComponent } from './log-in/log-in.component';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, LogInComponent]
 })
 export class AppModule { }
---
+++ src/app/log-in/log-in.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.scss']
+})
+export class LogInComponent {
+
+  readonly username: String = 'admin';
+  readonly password: String = 'admin';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  log_in(username: String, uassword:String) {
+    if (username == this.username && uassword == this.password) {
+      alert('Login successful');
+      document.getElementById('login-form').value = '';
+    }
+    else {
+      alert('Login failed');
+    }
+  }
+}
